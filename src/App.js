56a1a40ae228b15160370ff9bd26b1820e2d0767
unknown <sombradoble3@sombradoble.es>
2020-08-10 16:25:19 +0200

    primer commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          comete una mierda bolliquis
+          Comete una mierda gudi
         </p>
         <a
           className="App-link"
