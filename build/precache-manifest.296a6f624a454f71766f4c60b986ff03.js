e50c32f875cc503faa9ee49fcef0038c3fc0a031
unknown <sombradoble3@sombradoble.es>
2020-08-11 03:27:17 +0200

    changes

---
+++ build/precache-manifest.296a6f624a454f71766f4c60b986ff03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c2136df28c9c17b02f0241273f22aa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "160adb8928732a9d674b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "069aed24534ef2f50250",
+    "url": "/static/js/2.a1c894a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a1c894a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "160adb8928732a9d674b",
+    "url": "/static/js/main.717374c2.chunk.js"
+  },
+  {
+    "revision": "d98d8112ca80e5704569",
+    "url": "/static/js/runtime-main.135d0715.js"
+  }
+]);
