2f6b1b9e09352f95dc61aabcd782910e79aa028e
unknown <sombradoble3@sombradoble.es>
2020-08-12 16:54:06 +0200

    cambios proyeectos

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -6,25 +6,29 @@ export class Services extends Component {
       <div id="services" className="text-center">
         <div className="container">
           <div className="section-title">
-            <h2>Our Services</h2>
+            <h2>Proyectos</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-              dapibus leonec.
+              Listado de proyectos propios creados con diferentes tecnologías
+              para el aprendizaje.
             </p>
           </div>
           <div className="row">
-            {this.props.data
-              ? this.props.data.map((d, i) => (
-                  <div  key={`${d.name}-${i}`} className="col-md-4">
-                    {" "}
-                    <i className={d.icon}></i>
-                    <div className="service-desc">
-                      <h3>{d.name}</h3>
-                      <p>{d.text}</p>
-                    </div>
-                  </div>
-                ))
-              : "loading"}
+            <div className="col-md-4" style={{ textAlign: "center" }}>
+              {" "}
+                <img
+                src="img/projects/react.png"
+                style={{ height: 100, textAlign: "center" }}
+                alt="React"
+              />{" "}
+              <div className="service-desc">
+                <h3>React + Loopback</h3>
+                <p>
+                  Ejercicio de una tienda donde se agregan productos a un carro
+                  (Redux). Puedes ver el listado de productos y tu carro
+                  personal
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
